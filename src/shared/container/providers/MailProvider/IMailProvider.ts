8d819edfc893fc76999883587f3aea7549861347
matheusrgg <matheusrgg@gmail.com>
2021-09-23 10:52:08 -0300

    fim recuperação de senha

---
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -0,0 +1,11 @@
+interface IMailProvider {
+    sendMail(
+      to: string,
+      subject: string,
+      variables: any,
+      path: string
+    ): Promise<void>;
+  }
+  
+  export { IMailProvider };
+  
